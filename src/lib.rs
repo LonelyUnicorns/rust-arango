ad758aae64b5ba36b8aa6d9820168df688caf43b
anlaakso <an.laaakso@gmail.com>
2017-12-04 23:00:49 +0300

    Add entry point

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#![feature(conservative_impl_trait)]
+
+/**
+	@author anlaakso (Andrew)
+	@license GNU/GPLv3
+*/
+
+extern crate serde_json;
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[macro_use]
+extern crate typed_builder;
